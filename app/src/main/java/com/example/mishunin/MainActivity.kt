2c7c3c63b05778f059fb59088d5a3dcd12dac50a
FeN0-MEN <delsin77@mail.ru>
2025-02-17 18:40:09 +0500

    Branch-three-commit

--- app/src/main/java/com/example/mishunin/MainActivity.kt
+++ app/src/main/java/com/example/mishunin/MainActivity.kt
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MishuninTheme {
-        Greeting("Branch-one")
+        Greeting("From branch-three")
     }
 }
